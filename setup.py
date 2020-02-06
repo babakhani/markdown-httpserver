1a0c258bc98a3a8917fe6249e8817648303726aa
babakhani <babakhani.reza@gmail.com>
2020-02-07 00:51:12 +0330

    initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+import re
+from os.path import dirname, join
+
+from setuptools import setup, find_packages
+
+
+with open(join(dirname(__file__), 'markdownserver', '__init__.py')) as f:
+    version = re.match(r'^__version__ = \'(.*)\'', f.read()).group(1)
+
+
+dependencies = []
+
+
+setup(
+    name='markdown-httpserver',
+    version=version,
+    url='https://github.com/babakhani/markdown-httpserver',
+    author='Reza Babakhani',
+    author_email='babakhani.reza@gmail.com',
+    description='markdown http server',
+    packages=find_packages(),
+    install_requires=dependencies,
+)
