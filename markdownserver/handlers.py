aa7f692a57d61124551e6af3d162d1ec60e180bb
babakhani <babakhani.reza@gmail.com>
2020-02-10 01:33:33 +0330

    fix mako path

--- markdownserver/handlers.py
+++ markdownserver/handlers.py
@@ -1,7 +1,8 @@
-from os.path import join, exists
+from os.path import join, exists, dirname
 
 from yhttp import Application, text, notfound, html
 from mako.template import Template
+from mako.lookup import TemplateLookup
 
 from .cli import Version
 from .markdown import markdown2html
@@ -12,8 +13,14 @@ app = Application()
 app.cliarguments.append(Version)
 app.settings.merge('''
 root: .
+templatedir: 'mako'
 ''')
-template = Template(filename='template.mako')
+templatelookup = TemplateLookup(
+    directories=[join(dirname(__file__), app.settings.templatedir)],
+    output_encoding='utf-8',
+    encoding_errors='replace'
+)
+template = templatelookup.get_template("master.mako")
 
 
 @app.route(r'/(.*)')
@@ -21,7 +28,10 @@ template = Template(filename='template.mako')
 def get(req, path):
     root = app.settings.root
     if not path:
-        yield template.render(content=indexer.generate(root), settings=app.settings)
+        yield template.render(
+            content=indexer.generate(root),
+            settings=app.settings
+        )
         return
 
     filename = join(root, f'{path}.md')
@@ -29,4 +39,7 @@ def get(req, path):
         raise notfound()
 
     with open(filename) as f:
-        yield template.render(content=markdown2html(f.read()), settings=app.settings)
+        yield template.render(
+            content=markdown2html(f.read()),
+            settings=app.settings
+        )
